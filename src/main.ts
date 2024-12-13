ec281b99cc0b90b4f77c51896d36fa777922c358
Mustafa J <mjebbar17@gmail.com>
2024-12-13 17:59:23 +0000

    Integrate Winston logging with NestJS; add custom HTTP exception filter and logger configuration. Update dependencies in package.json and package-lock.json to include nest-winston and winston.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { WinstonModule } from 'nest-winston';
+import { loggerConfig } from './common/config/logger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(loggerConfig),
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,6 +16,8 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
